eb471c145198dccea4a4ad42437f83553f701292
sivar2311 <sivar2311@gmail.com>
2024-04-04 14:08:25 +0200

    Update fileA.cpp

--- src/fileA.cpp
+++ src/fileA.cpp
@@ -1,1 +1,10 @@
 #include "fileA.h"
+
+AsyncWebServer server(80);
+
+void setupWebServer() {
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/plain", "Hello, world");
+    });
+    server.begin();
+}
